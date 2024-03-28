1148e7747c14dd6d861f50e34618c62c0e6dff4d
Eric Viera <ericviera@Erics-MacBook-Pro.local>
2024-03-28 11:24:09 -0400

    update dev

--- TapCoins_Server/settings.py
+++ TapCoins_Server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['35.171.186.53']
+ALLOWED_HOSTS = []
 
 
 # Application definition
